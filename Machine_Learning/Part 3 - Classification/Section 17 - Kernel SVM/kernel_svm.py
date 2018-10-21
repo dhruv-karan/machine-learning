46f9c9f840e5a2fcfa1fe5ad382a7218fc01c24e
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-21 11:39:10 +0530

    performed kernel svm on impoerted dat set and  did data visualing

--- Machine_Learning/Part 3 - Classification/Section 17 - Kernel SVM/kernel_svm.py
+++ Machine_Learning/Part 3 - Classification/Section 17 - Kernel SVM/kernel_svm.py
@@ -1,4 +1,4 @@
-# Kernel SVM
+# Classification template
 
 # Importing the libraries
 import numpy as np
@@ -20,10 +20,10 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Kernel SVM to the Training set
+# Fitting classifier to the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(X_train, y_train)
+classifier = SVC(kernel='rbf',random_state=0)
+classifier.fit(X_train,y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -44,7 +44,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Training set)')
+plt.title('Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -62,7 +62,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Test set)')
+plt.title('Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
