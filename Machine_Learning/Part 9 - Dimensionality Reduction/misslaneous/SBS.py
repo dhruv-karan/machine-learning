bcc78a59a3ccae224f978b540405227f3917bc19
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-13 20:20:31 +0530

    verified SBS algorithum using KNN algorithum

--- Machine_Learning/Part 9 - Dimensionality Reduction/misslaneous/SBS.py
+++ Machine_Learning/Part 9 - Dimensionality Reduction/misslaneous/SBS.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+
+# ====== SBS ======== sequential backward propagation =====================
 from sklearn.base import clone
 from itertools import combinations
 import numpy as np
-from sklearn.cross_validation import accuracy_score
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
 
-class Sbs():
-    def __init__(self,estimator,k_features,scoring=accuracy_score,test_size,random_state=1):
+class SBS():
+    def __init__(self,estimator,k_features,scoring=accuracy_score,test_size=0.25,random_state=1):
         self.scoring = scoring
         self.estimator = clone(estimator)
         self.f_features = k_features
@@ -16,10 +19,10 @@ class Sbs():
         X_train,X_test,y_train,y_test = \
                 train_test_split(X,y,test_size=self.test_size,random_state = self.random_state)
         
-        dim = X_train.shape[1]
+        dim = X_train.shape[0]
         self.indices_ = tuple(range(dim))
         self.subsets_ = [self.indices_]
-        score = self._calc_score(X_train,y_train,X_test,y_test,self.indices_)
+        score = self._cal_score(X_train,y_train,X_test,y_test,self.indices_)
         
         self.scores_ = [score]
         
@@ -34,7 +37,7 @@ class Sbs():
             
             best = np.argmax(scores)
             self.indices_ = subsets[best]
-            self.subsets_.append(self_indices_)
+            self.subsets_.append(self.indices_)
             dim -=1
             self.score_append(scores[best])
         self.k_score_ = self.scores_[-1]
@@ -45,13 +48,36 @@ class Sbs():
         return X[:,self.indices_]
     
     def _cal_score(self,X_train,y_train,X_test,y_test,indices):
-        self.estimator.fit(X_trainp[:,indices],y_train)
+        self.estimator.fit(X_train[:,indices],y_train)
         y_pred =self.estimator.predict(X_test[:,indices])
-        score = self.scoring(y_test,y_test)
+        score = self.scoring(y_test,y_pred)
         return score
         
         
+#==================== Varfying using KNN classifier
         
+# -*- coding: utf-8 -*-
+
+
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+X = np.random.random((2000))
+y = np.random.random((2000))
+knn = KNeighborsClassifier(n_neighbors=2)
+sbs = SBS(knn, k_features=1)
+sbs.fit(X,y)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs._cal_scores_, marker='o')
+plt.ylim([0.7,1.1])
+
+plt.ylabel('Accuracy')
+plt.xlabel('number of features')
+plt.grid()
+plt.show()
+
+
+X.shape[1]
         
         
         
